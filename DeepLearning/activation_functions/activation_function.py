49f6961e30141efed35ccd050247e007bda81226
aiien61 <aiien61will@gmail.com>
2025-06-04 20:21:04 +0800

    relu

--- DeepLearning/activation_functions/activation_function.py
+++ DeepLearning/activation_functions/activation_function.py
@@ -4,6 +4,9 @@ import matplotlib.pylab as plt
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def relu(x):
+    return np.maximum(0, x)
+
 
 if __name__ == "__main__":
     x: np.ndarray = np.arange(-5, 5, 0.1)
